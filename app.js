e6410d656092fd598a9ea9ca839509d424d10fca
sladeh <s.hulderic@gmail.com>
2014-05-06 23:54:32 +0200

    Tried new methods, didn't work. Added compiled crx file, cleaned up the code.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 /*
-* Share e621 images to Google+, only direct links and no
-* image embedding self. This is a Google issue.
-*
-* Copyright suck my dick.
-* Follow me at: https://plus.google.com/111574610177008585768/posts :^)
+	Share e621 images to Google+, only direct links and no
+	image embedding self. This is a Google issue.
+	Uses jQuery and stuff, so keep yourself to those copyrights.
+
+	Abandonware tho!
 */
+
+// $('head').append('<meta property=\"og:title" content=\"imgur: the simple image sharer\">');
+// $('head').append('<meta property=\"og:image" content=\"' + url + '\">');
+// $('head').append('<meta property=\"og:type" content=\"article\">');
 
 var url = $("a:contains('Download')").attr("href");
 
-$("h4").append("| <div id=\"replaceme\"></div>");
-$("#replaceme").replaceWith("<span id=\"gplus\">Share to G+</span>");
+$("h4").append("| <span id=\"gplus\">Share to G+</span>");
 $("#gplus").css({
 				"cursor": "pointer",
 				"color": "#b4c7d9"
@@ -23,17 +26,5 @@ $("#gplus").on("mouseenter", function() {
 });
 
 $("#gplus").click(function() {
-	$("#gplus").replaceWith("<span id=\"sharing\">Sharing...</span>");
-	$("#sharing").append("<img src=" + chrome.extension.getURL("ajax-loader.gif") + " />");
-	chrome.extension.sendRequest({method: 'PlusAPI', data: {service: 'Plus', method: 'init'}}, function(initResponse) {
-		console.log(initResponse);
-		chrome.extension.sendRequest({method: 'PlusAPI', data: {service: 'plus', method: 'newPost', content: 'test'}}, function(postResponse) {
-			if(postResponse !== false) {
-				$("#sharing").replaceWith("<span id=\"success\">Shared to G+!</span>");
-			} else if (postResponse == false) {
-				$("#sharing").replaceWith("<span id=\"failure\">Oops, something went wrong!</span>");
-			}
-			console.log(postResponse);
-		});
-	});
+	window.open("https://plus.google.com/share?url=" + url, "Share to G+", "width=500,height=500");
 });
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-E621ToGooglePlus
+# E621ToGooglePlus
 ================
-
 A Google Chrome extension to share E621 images directly to Google Plus.
 
-
-How to use
-================
+## How to use
 Drag and drop the crx file in chrome extensions screen.
 Go to e621 and view an image, a new button should be added below the image next to "Download".
+
+
+Copyrights for the icon go to the original creator.
